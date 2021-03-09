170c05dbf10b58b4bf90ae1a05c01845ce5d89e8
Alauddin Al Azad <saimon.azad@hotmail.com>
2021-03-10 03:41:56 +0600

    error fix

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 //file import from
-import App from './app'
+import App from './App'
 
 ReactDOM.render(<App/>, document.querySelector('#root'))
